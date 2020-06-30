2ddb6375cb24534b1353abc4fcffd8ba5eda0571
Frank de Jonge <info@frenky.net>
2020-06-30 22:18:41 +0200

    Rename continued

--- src/event-sourcing/producing-message-repository.ts
+++ src/event-sourcing/producing-message-repository.ts
@@ -2,7 +2,7 @@ import { EventStreamDefinition } from './interfaces';
 import { MessageRepository } from './message-repository';
 import { AnyMessageFrom, MessageProducer, MessagesFrom } from '../messaging';
 
-export class DispatchingMessageRepository<Stream extends EventStreamDefinition<Stream>> implements MessageRepository<Stream> {
+export class ProducingMessageRepository<Stream extends EventStreamDefinition<Stream>> implements MessageRepository<Stream> {
     constructor(
         private readonly repository: MessageRepository<Stream>,
         private readonly dispatcher: MessageProducer<Stream>
